032cb483940999e0a07345a0ac370eb1d00930b2
kisaragui <njesusas@gmail.com>
2023-05-12 04:18:58 -0300

    Update filter.py

--- dags/transform/filter.py
+++ dags/transform/filter.py
@@ -7,9 +7,7 @@ def consigna(df):
       try:   
             data =  pd.DataFrame.from_dict(json.loads(df), orient ='columns')
             query = data.groupby(["STNAME", "YEAR"])["AAWDT"].count()
-            print(query)
             convert = query.reset_index().to_json(orient = 'columns')
-            print("Total: {} registers grouped.".format(len(query))) 
       except:
             query = print("No se pudo leer el archivo")
       return convert
